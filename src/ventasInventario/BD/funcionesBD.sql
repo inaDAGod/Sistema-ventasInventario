a6e5c37a821d1996db97f347cf0214499f09f5dd
inaDAGod <danialee14@gmail.com>
2023-11-23 13:11:26 -0600

    registrar funcionario BD

--- src/ventasInventario/BD/funcionesBD.sql
+++ src/ventasInventario/BD/funcionesBD.sql
@@ -7,4 +7,15 @@ $crearUsuario$
 language plpgsql;
 
 --select registrarse('danialee14','dani','danialee14@gmail.com','admin123');
+
+
+create or replace function registrarFuncionario(usuario varchar(30), nombre varchar(50),correo varchar(40),contrasenia varchar(20))
+returns void as $crearFuncionario$
+begin
+    insert into usuarios values (usuario,nombre,correo,contrasenia,TRUE, FALSE);
+end;
+$crearFuncionario$
+language plpgsql;
+
+--select registrarFuncionario('funci','funcionario','funcionario@gmail.com','admin123');
 --select * from usuarios;
