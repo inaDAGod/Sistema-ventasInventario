fc9dd8ec0ce7cce4404014190f225d6257aae77f
inaDAGod <danialee14@gmail.com>
2023-11-25 11:57:06 -0400

    clases POO

---
+++ src/ventasInventario/POO/Carrito.java
@@ -0,0 +1,49 @@
+package ventasInventario.POO;
+
+import java.util.*;
+
+public class Carrito {
+	private Usuario usuario;
+	private ArrayList<ProductoCarrito> productos; 
+	private Double total;
+	
+	public Carrito(Usuario usuario) {
+		this.usuario = usuario;
+		this.productos = new ArrayList();
+		this.total = 0.0;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public ArrayList<ProductoCarrito> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(ArrayList<ProductoCarrito> productos) {
+		this.productos = productos;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	@Override
+	public String toString() {
+		return "Carrito [usuario=" + usuario + ", productos=" + productos + ", total=" + total + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/ventasInventario/POO/ProductosPedido.java
@@ -0,0 +1,11 @@
+package ventasInventario.POO;
+
+public class ProductosPedido extends ProductoCarrito{
+
+	public ProductosPedido(Producto producto, Integer cantidad) {
+		super(producto, cantidad);
+	}
+	
+	
+	
+}
